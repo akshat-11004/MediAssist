065361d2789da86708aa003af1cbcffb8ee8c7a0
akshat-11004 <akshatkadia02@gmail.com>
2023-11-12 19:03:42 +0530

    Add medicine in pharmacy controller

--- data.sql
+++ data.sql
@@ -73,9 +73,9 @@ CREATE TABLE stores(
     name VARCHAR(255),
     brand_name VARCHAR(255),
     stock INT,
-    PRIMARY KEY(email_pharm,medicine_name,brand_name),
+    PRIMARY KEY(email_pharm,name,brand_name),
     FOREIGN KEY(email_pharm) REFERENCES Pharmacy(email),
-    FOREIGN KEY(medicine_name,brand_name) REFERENCES Medicine
+    FOREIGN KEY(name,brand_name) REFERENCES Medicine
 );
 
 --Access
@@ -122,7 +122,7 @@ CREATE TABLE Ambulance_driver(
     email VARCHAR(255) PRIMARY KEY,
     contact_number  CHAR(10), 
     licence  CHAR(16) NOT NULL,
-    vehicle_number VARCHAR(16) NOT NULL,
+    vehicle_number VARCHAR(16) NOT NULL
 );
 
 --Laboratory table
@@ -158,7 +158,7 @@ CREATE TABLE appoints (
 CREATE TABLE Medicine(
     name VARCHAR(255),
     brand_name VARCHAR(255),
-    PRIMARY KEY(medicine_name, brand_name)
+    PRIMARY KEY(name, brand_name)
 );
 
 -- Pharmacy Table
